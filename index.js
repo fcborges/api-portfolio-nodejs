8bf5d32f643d824415cf977e443fca5b3e52c027
fcborges <fatimacarvalhob@gmail.com>
2020-03-15 23:12:04 -0300

    l

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDiivFGix_Ue62gcC0SaN-JZrtieq-B-5w",
-    authDomain: "api-node-dasa.firebaseapp.com",
-    databaseURL: "https://api-node-dasa.firebaseio.com",
-    projectId: "api-node-dasa",
-    storageBucket: "api-node-dasa.appspot.com",
-    messagingSenderId: "102363352298",
-    appId: "1:102363352298:web:568b5d5b48e097e6599b93"
-  };
-
 conexao.connect(erro => {
     if(erro) {
         console.log(erro)
